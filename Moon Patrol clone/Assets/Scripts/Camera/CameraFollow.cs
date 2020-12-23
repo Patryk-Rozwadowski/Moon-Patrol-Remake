c97eea8c93d4739db2e84e470a18bd91c8b3b2b3
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-12-23 19:28:22 +0100

    Fajny komit

--- Moon Patrol clone/Assets/Scripts/Camera/CameraFollow.cs
+++ Moon Patrol clone/Assets/Scripts/Camera/CameraFollow.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        Debug.Log("yoyoy");
     }
 }
