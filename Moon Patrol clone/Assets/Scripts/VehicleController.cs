466af2bf489f873f748fe7db3ecf31f2c69134a8
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-12-23 12:03:09 +0100

    basic vehicle movement controller, basic terrain, add placeholder sprites

---
+++ Moon Patrol clone/Assets/Scripts/VehicleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour {
+    [SerializeField] Rigidbody2D backTire;
+    [SerializeField] Rigidbody2D frontTire;
+
+    [SerializeField] private float speed = 200;
+    private float movement;
+
+    private void Start() {
+    }
+
+    
+    
+    void Update() {
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate() {
+        backTire.AddTorque(-speed * Time.fixedDeltaTime);
+    }
+}
