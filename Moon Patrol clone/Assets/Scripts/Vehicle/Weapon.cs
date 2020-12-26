dfc58c3a755fd2993844a6d6cf18b5f6e0fa4f88
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-12-26 23:55:46 +0100

    Add basic script for shooting, serializedfield for dynamic shoot keycodes

---
+++ Moon Patrol clone/Assets/Scripts/Vehicle/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    public Transform firePoint;
+    [SerializeField] KeyCode horizonFireKey = KeyCode.None;
+    [SerializeField] KeyCode verticalFireKey = KeyCode.None;
+
+    void Update() {
+        if (Input.GetKeyDown(horizonFireKey)) {
+            HorizontalShoot();
+        } else if (Input.GetKeyDown(verticalFireKey)) {
+            VerticalShoot();
+        }
+    }
+
+    private void HorizontalShoot() {
+        Debug.Log("Horizontal shoot");
+    }
+
+    private void VerticalShoot() {
+        Debug.Log("Vertical shoot");
+    }
+}
