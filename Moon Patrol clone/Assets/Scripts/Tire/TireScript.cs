35b5f7f105c29182f3df33872f4063b0e8eff71f
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-12-30 15:58:52 +0100

    Basic jump script with colliders with locked rotation

---
+++ Moon Patrol clone/Assets/Scripts/Tire/TireScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TireScript : MonoBehaviour {
+    private void Start() {
+        Debug.Log("Collider");
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        VehicleController.isInAir = true;
+        Debug.Log("Tire in air");
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        VehicleController.isInAir = false;
+        Debug.Log(other.name);
+        Debug.Log("Tire on ground");
+    }
+}
