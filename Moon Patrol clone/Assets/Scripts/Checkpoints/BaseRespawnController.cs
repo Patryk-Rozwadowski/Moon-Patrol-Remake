a5cd5d9da24f8a24abc5f29d8c2b1ffc27beb506
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-04 01:10:01 +0100

    add baseRespawn with scriptableobjects

---
+++ Moon Patrol clone/Assets/Scripts/Checkpoints/BaseRespawnController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseRespawnController : MonoBehaviour {
+    [SerializeField] private BaseRespawnSO sprite = null;
+
+
+    private void Start() {
+        GetComponent<SpriteRenderer>().sprite = sprite.baseSprite;
+    }
+}
---
+++ Moon Patrol clone/Assets/Scripts/Checkpoints/BaseRespawnSO.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/BaseRespawn")]
+public class BaseRespawnSO : ScriptableObject {
+    [SerializeField] public Sprite baseSprite = null;
+}
