b01fe65e51d8764f1a35488e196b26cd72bc2b1d
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-06 01:02:18 +0100

    Fixed base respawn

--- Moon Patrol clone/Assets/Scripts/Checkpoints/BaseRespawnController.cs
+++ Moon Patrol clone/Assets/Scripts/Checkpoints/BaseRespawnController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class BaseRespawnController : MonoBehaviour {
     [SerializeField] private BaseRespawnSO sprite = null;
-
+    [SerializeField] private GameObject vehicle = null;
 
     private void Start() {
         GetComponent<SpriteRenderer>().sprite = sprite.baseSprite;
+        Instantiate(vehicle, gameObject.transform.transform);
     }
 }
