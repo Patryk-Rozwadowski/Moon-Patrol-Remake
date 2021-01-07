9a2eceb1cd001ae3d557c32aed0bb7d4b75d64f1
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-07 23:33:55 +0100

    add basic enemy (tank) shooting and player death

--- Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyController.cs
+++ Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyController.cs
@@ -1,11 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
     [SerializeField] private GameObject deathEffect = null;
+    [SerializeField] private Transform firepoint = null;
+    [SerializeField] private Transform playerPos = null;
+    [SerializeField] private EnemyParamsSO enemyParams = null;
+    [SerializeField] private GameObject enemyHorizontalBullet = null;
+
+    private float fireRate = 1f;
+    private float nextFire = 0f;
+    
+    private void Start() {
+        Debug.Log($"Enemy vision: {enemyParams.visionRange}");
+    }
+    private void Update() {
+        if (playerPos.position.x < enemyParams.visionRange && Time.time > nextFire) { 
+            Shooting();
+        }
+    }
+    
     public void EnemyDeath() {
         Instantiate(deathEffect, transform.position, Quaternion.identity);
+        GetComponent<SpriteRenderer>().sprite = enemyParams.sprite;
         Destroy(gameObject);
+    }
+
+    private void Shooting() {
+        nextFire = Time.time + fireRate;
+        Instantiate(enemyHorizontalBullet, firepoint.position, Quaternion.identity);
     }
 }
---
+++ Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyHorizonBulletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHorizonBulletController : MonoBehaviour {
+    [SerializeField] private Rigidbody2D bulletRigidBody = null;
+    [SerializeField] private ProjectileSpeedSO projectileSpeed = null;
+
+    private void Start() {
+        bulletRigidBody.velocity = new Vector2(-projectileSpeed.projectileSpeed, 0);
+    }
+
+    private void OnDestroy() {
+        Debug.Log($"{gameObject.name} Destroyed");
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj) {
+        Destroy(gameObject);
+        VehicleController player = obj.GetComponent<VehicleController>();
+        Debug.Log($"Enemy hit: {obj} ");
+        if (player == null) return;
+        player.PlayerDeath();
+        Debug.Log($"Vertical Bullet hit: {obj.name}");
+    }
+}
