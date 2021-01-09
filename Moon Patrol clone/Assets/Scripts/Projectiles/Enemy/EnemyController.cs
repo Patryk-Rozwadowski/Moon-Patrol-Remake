1a2a79a66e7523dbfc499d86d4ab36c66dc88ee9
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-09 11:41:18 +0100

    project clean up

--- Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyController.cs
+++ Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyController.cs
@@ -11,18 +11,18 @@ public class EnemyController : MonoBehaviour {
 
     private float fireRate = 0.5f;
     private float nextFire = 0f;
-    
+
     private void Start() {
         Debug.Log($"Enemy vision: {enemyParams.visionRange}");
     }
-    private void Update()
-    {
+
+    private void Update() {
         if (playerPos == null) return;
-        if (playerPos.position.x < enemyParams.visionRange && Time.time > nextFire) { 
+        if (playerPos.position.x < enemyParams.visionRange && Time.time > nextFire) {
             Shooting();
         }
     }
-    
+
     public void EnemyDeath() {
         Instantiate(deathEffect, transform.position, Quaternion.identity);
         GetComponent<SpriteRenderer>().sprite = enemyParams.sprite;
