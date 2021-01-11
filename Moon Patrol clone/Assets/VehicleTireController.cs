703c56c0e208832a52796024a5bf18b7291e2f47
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-11 23:42:19 +0100

    add jumping cooldown, better jumping

--- Moon Patrol clone/Assets/VehicleTireController.cs
+++ Moon Patrol clone/Assets/VehicleTireController.cs
@@ -1,40 +1,30 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class VehicleTireController : MonoBehaviour {
-    [SerializeField] private Rigidbody2D tireRigidbody2D = null;
+    [SerializeField] private Rigidbody2D tireRigidbody2D;
     
     private bool jumping = false;
-    private float jumpTime = 1.5f;
+    
+    private float jumpTime = 0f;
+    private float jumpHeight = 2f;
+
+    private float coolDown = 3f;
+    
     private float nextJump = 0f;
     private float pos = 0f;
     private void Start() {
         pos = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down)).collider.transform.position.y;
     }
 
-    void Update() {
-        
-        if (Input.GetKeyDown(KeyCode.D) && nextJump < jumpTime) {
-            nextJump = Time.time + 2;
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.D) && Time.time > nextJump) {
+            jumpTime = Time.time + jumpHeight;
+            nextJump = Time.time + coolDown + jumpHeight;
         }
-        
-        if ( Time.time < nextJump) {
-            tireRigidbody2D.velocity = new Vector2(5f, 1f);
-            Debug.Log($"Jumtime in jump: {nextJump}");
-        }
-        else {
-            if (nextJump != 0 && nextJump >= jumpTime) {
-                nextJump -= Time.time;
-                Debug.Log($"MORE THAN JUMPTIME {nextJump}");
-            }
-            
-            Debug.Log(nextJump);
-            tireRigidbody2D.velocity = new Vector2(10f, pos);
-            
-            // Debug.Log(pos);
+        else if (Time.time < nextJump){
+            Debug.Log($"COOLDOWN: {nextJump - Time.time}");
         }
 
+        tireRigidbody2D.velocity = Time.time < jumpTime ? new Vector2(5f, 1f) : new Vector2(10f, pos);
     }
 }
