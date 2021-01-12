6b7bd24c45def62cbe7899f72fc43b172b616947
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-12 23:50:41 +0100

    Keyboard change location, rename friction material

--- Moon Patrol clone/Assets/Scripts/Player/ScriptableObjects/Speed/PlayerSpeedSO.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObjects/Player/Speed")]
-public class PlayerSpeedSO : ScriptableObject {
-    [SerializeField] public float playerSpeed = 300f;
-}
