dfb81ddf5dce12b01672ce0724f9fb30e65a5945
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-13 17:14:41 +0100

    stage timer ui

---
+++ Moon Patrol clone/Assets/Scripts/UI/StageTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageTime : MonoBehaviour {
+    [SerializeField] private Text stageTimer;
+
+    private float _timer;
+
+    private void Start() {
+        _timer = 0;
+    }
+
+    void Update() {
+        _timer += Time.deltaTime;
+        int seconds = Mathf.FloorToInt(_timer % 60F);
+        stageTimer.text = seconds.ToString("000");
+    }
+
+}
