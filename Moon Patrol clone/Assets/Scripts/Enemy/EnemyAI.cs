a889bb4b398cb7bc4683a3ad9f3ae58c7140db1b
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-18 23:48:20 +0100

    Add gizmos for next move, created basic for ai walls

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class EnemyAI : MonoBehaviour {
@@ -11,26 +12,43 @@ public class EnemyAI : MonoBehaviour {
     private Vector2 _movementDirection;
     private Vector2 _movementPerSecond;
 
-    private void Start(){
+    private Vector2 _currentPosition;
+    private Vector2 _targetPosition;
+
+    private void Start() {
         _latestDirectionChangeTime = 0f;
         CalcuateNewMovementVector();
     }
- 
-    private void CalcuateNewMovementVector(){
-        _movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+
+    private void CalcuateNewMovementVector() {
+        _movementDirection = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f)).normalized;
         _movementPerSecond = _movementDirection * _characterVelocity;
+
+        _targetPosition = new Vector2(_movementDirection.x + transform.position.x + 5f,
+            _movementDirection.y + transform.position.y);
     }
- 
-    void Update(){
+
+    void Update() {
         rigidbody2d.velocity = new Vector2(5f, 0);
-        if (Time.time - _latestDirectionChangeTime > _directionChangeTime){
+
+        if (Time.time - _latestDirectionChangeTime > _directionChangeTime) {
             _latestDirectionChangeTime = Time.time;
             CalcuateNewMovementVector();
         }
-     
-        transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime), 
+
+        transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
             transform.position.y + (_movementPerSecond.y * Time.deltaTime));
- 
     }
-    
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, _targetPosition);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other);
+        if (other.name == "AiWalls") {
+            CalcuateNewMovementVector();
+        }
+    }
 }
