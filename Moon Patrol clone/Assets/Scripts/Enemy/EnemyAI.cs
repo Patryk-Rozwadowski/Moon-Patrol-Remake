1cb785d07d7b069f9b1aac1b8c416fe44b70e458
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-19 08:49:01 +0100

    add reverse ai move

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
@@ -7,35 +7,60 @@ public class EnemyAI : MonoBehaviour {
     [SerializeField] private Rigidbody2D rigidbody2d;
 
     private float _latestDirectionChangeTime;
-    private readonly float _directionChangeTime = 1f;
-    private float _characterVelocity = 2f;
+    private readonly float _directionChangeTime = 0.5f;
+    private float _characterVelocity = 12f;
+    
     private Vector2 _movementDirection;
     private Vector2 _movementPerSecond;
 
     private Vector2 _currentPosition;
     private Vector2 _targetPosition;
 
+
+    private bool reverse = false;
     private void Start() {
         _latestDirectionChangeTime = 0f;
         CalcuateNewMovementVector();
     }
 
     private void CalcuateNewMovementVector() {
-        _movementDirection = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f)).normalized;
+        Debug.Log("CALCULATE NEW MOVEMENT VECTOR");
+        if(reverse == true) {
+            _movementDirection = -_movementDirection;
+            _movementPerSecond = _movementDirection * _characterVelocity;
+            _targetPosition = new Vector2(_movementDirection.x + 5f,
+                _movementDirection.y + 5f);
+            return;
+        }
+        _movementDirection = new Vector2(Random.Range(-12f, 22f), Random.Range(-1f, 1f)).normalized;
         _movementPerSecond = _movementDirection * _characterVelocity;
-
-        _targetPosition = new Vector2(_movementDirection.x + transform.position.x + 5f,
-            _movementDirection.y + transform.position.y);
+        reverse = false;
+        
+        _targetPosition = new Vector2(-_movementDirection.x + 5f,
+            -_movementDirection.y + 5f);
     }
 
     void Update() {
+        // TODO remove hardcoded speed
         rigidbody2d.velocity = new Vector2(5f, 0);
 
-        if (Time.time - _latestDirectionChangeTime > _directionChangeTime) {
+        if (Time.time - _latestDirectionChangeTime > _directionChangeTime && reverse == false) {
             _latestDirectionChangeTime = Time.time;
             CalcuateNewMovementVector();
+            reverse = false;
+            
+            transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
+                transform.position.y + (_movementPerSecond.y * Time.deltaTime));
         }
-
+        
+        if (Time.time - _latestDirectionChangeTime > _directionChangeTime && reverse) {
+            Debug.Log("REVERSE");
+            _latestDirectionChangeTime = Time.time;
+            CalcuateNewMovementVector();
+            reverse = false;
+            transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
+                transform.position.y + (_movementPerSecond.y * Time.deltaTime));
+        }
         transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
             transform.position.y + (_movementPerSecond.y * Time.deltaTime));
     }
@@ -46,10 +71,10 @@ public class EnemyAI : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log(other);
         if (other.name == "AiWalls") {
+            Debug.Log($"AI WALLS TIME {_latestDirectionChangeTime}");
+            reverse = true;
             Debug.Log("AI WALL");
-            _latestDirectionChangeTime = 0f;
             CalcuateNewMovementVector();
         }
     }
