c80209d0c2ba5b8db2cd311ef956d194ef9b519a
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-20 00:11:36 +0100

    horizontal ai movement

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class EnemyAI : MonoBehaviour {
-    [SerializeField] private Rigidbody2D rigidbody2d;
-
     private float _latestDirectionChangeTime;
     private float _directionChangeTime = 1f;
-    private float _characterVelocity = 5f;
+    private float _characterVelocity = 3f;
     
     private Vector2 _movementDirection;
     private Vector2 _movementPerSecond;
 
-    private Vector2 _currentPosition;
-    private Vector2 _targetPosition;
 
-    private Vector2 _lastVector;
-
-    // private bool reverse = false;
     private void Start() {
         _latestDirectionChangeTime = 0f;
         Debug.LogWarning(_latestDirectionChangeTime);
@@ -26,17 +18,9 @@ public class EnemyAI : MonoBehaviour {
     }
 
     private void CalcuateNewMovementVector(bool reverse) {
-        if(reverse) {
-            _movementDirection =  Vector3.Reflect(_movementDirection, -_movementDirection);
-            Debug.LogWarning($"REVERSE CALCULATE CURRENT: {_movementDirection.x} {_movementDirection.y}");
-            _movementPerSecond = _movementDirection * _characterVelocity;
-            return;
-        }
-        _movementDirection = new Vector2(Random.Range(-12f, 22f), Random.Range(-1f, 1f)).normalized;
-        _lastVector = _movementDirection;
+        _movementDirection = new Vector2(Random.Range(-1f, 1f), 0).normalized;
         _movementPerSecond = _movementDirection * _characterVelocity;
     }
-// TODO WYSKAKUJE POZA COLLIDER
     void Update() {
         if (Time.time - _latestDirectionChangeTime > _directionChangeTime) {
             _latestDirectionChangeTime = Time.time;
@@ -46,10 +30,12 @@ public class EnemyAI : MonoBehaviour {
             transform.position.y + (_movementPerSecond.y * Time.deltaTime));
     }
 
-    private void OnTriggerStay2D(Collider2D other) {
-        if (other.name == "AiWalls") {
-            Debug.Log("AI WALL");
-            CalcuateNewMovementVector(true);
+    private void OnTriggerStay2D(Collider2D col) {
+        if (col.name == "AiWalls") {
+            Debug.Log("reverse");
+            _movementDirection = -_movementDirection;
+            _movementPerSecond = _movementDirection * (_characterVelocity + 2);
+            _latestDirectionChangeTime = Time.time;
         }
     }
 }
--- Moon Patrol clone/Assets/Scripts/Utils/ConstantMoveFoward.cs
+++ Moon Patrol clone/Assets/Scripts/Utils/ConstantMoveFoward.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class ConstantMoveFoward : MonoBehaviour {
