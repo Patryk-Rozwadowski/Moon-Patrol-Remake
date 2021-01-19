2ac62c6b50a0f13815cac51496996f7e53f46421
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-19 22:30:46 +0100

    Basic Ai walls functionality

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
@@ -7,8 +7,8 @@ public class EnemyAI : MonoBehaviour {
     [SerializeField] private Rigidbody2D rigidbody2d;
 
     private float _latestDirectionChangeTime;
-    private readonly float _directionChangeTime = 0.5f;
-    private float _characterVelocity = 12f;
+    private float _directionChangeTime = 1f;
+    private float _characterVelocity = 5f;
     
     private Vector2 _movementDirection;
     private Vector2 _movementPerSecond;
@@ -16,66 +16,40 @@ public class EnemyAI : MonoBehaviour {
     private Vector2 _currentPosition;
     private Vector2 _targetPosition;
 
+    private Vector2 _lastVector;
 
-    private bool reverse = false;
+    // private bool reverse = false;
     private void Start() {
         _latestDirectionChangeTime = 0f;
-        CalcuateNewMovementVector();
+        Debug.LogWarning(_latestDirectionChangeTime);
+        CalcuateNewMovementVector(false);
     }
 
-    private void CalcuateNewMovementVector() {
-        Debug.Log("CALCULATE NEW MOVEMENT VECTOR");
-        if(reverse == true) {
-            _movementDirection = -_movementDirection;
+    private void CalcuateNewMovementVector(bool reverse) {
+        if(reverse) {
+            _movementDirection =  Vector3.Reflect(_movementDirection, -_movementDirection);
+            Debug.LogWarning($"REVERSE CALCULATE CURRENT: {_movementDirection.x} {_movementDirection.y}");
             _movementPerSecond = _movementDirection * _characterVelocity;
-            _targetPosition = new Vector2(_movementDirection.x + 5f,
-                _movementDirection.y + 5f);
             return;
         }
         _movementDirection = new Vector2(Random.Range(-12f, 22f), Random.Range(-1f, 1f)).normalized;
+        _lastVector = _movementDirection;
         _movementPerSecond = _movementDirection * _characterVelocity;
-        reverse = false;
-        
-        _targetPosition = new Vector2(-_movementDirection.x + 5f,
-            -_movementDirection.y + 5f);
     }
-
+// TODO WYSKAKUJE POZA COLLIDER
     void Update() {
-        // TODO remove hardcoded speed
-        rigidbody2d.velocity = new Vector2(5f, 0);
-
-        if (Time.time - _latestDirectionChangeTime > _directionChangeTime && reverse == false) {
+        if (Time.time - _latestDirectionChangeTime > _directionChangeTime) {
             _latestDirectionChangeTime = Time.time;
-            CalcuateNewMovementVector();
-            reverse = false;
-            
-            transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
-                transform.position.y + (_movementPerSecond.y * Time.deltaTime));
-        }
-        
-        if (Time.time - _latestDirectionChangeTime > _directionChangeTime && reverse) {
-            Debug.Log("REVERSE");
-            _latestDirectionChangeTime = Time.time;
-            CalcuateNewMovementVector();
-            reverse = false;
-            transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
-                transform.position.y + (_movementPerSecond.y * Time.deltaTime));
+            CalcuateNewMovementVector(false);
         }
         transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
             transform.position.y + (_movementPerSecond.y * Time.deltaTime));
     }
 
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, _targetPosition);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerStay2D(Collider2D other) {
         if (other.name == "AiWalls") {
-            Debug.Log($"AI WALLS TIME {_latestDirectionChangeTime}");
-            reverse = true;
             Debug.Log("AI WALL");
-            CalcuateNewMovementVector();
+            CalcuateNewMovementVector(true);
         }
     }
 }
