8eacf0d62369f03892861d607d0cf7a97b64a8f4
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-20 17:50:53 +0100

    Better ai, less collision passing through

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyAI.cs
@@ -5,37 +5,53 @@ using Random = UnityEngine.Random;
 public class EnemyAI : MonoBehaviour {
     private float _latestDirectionChangeTime;
     private float _directionChangeTime = 1f;
-    private float _characterVelocity = 3f;
+    private float _characterVelocity = 5f;
     
     private Vector2 _movementDirection;
     private Vector2 _movementPerSecond;
 
-
+    private bool reverse;
     private void Start() {
         _latestDirectionChangeTime = 0f;
         Debug.LogWarning(_latestDirectionChangeTime);
-        CalcuateNewMovementVector(false);
+        CalcuateNewMovementVector();
     }
 
-    private void CalcuateNewMovementVector(bool reverse) {
-        _movementDirection = new Vector2(Random.Range(-1f, 1f), 0).normalized;
-        _movementPerSecond = _movementDirection * _characterVelocity;
+    private void CalcuateNewMovementVector() {
+        _movementDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        _movementPerSecond = new Vector2(_movementDirection.x * _characterVelocity, _movementDirection.y/4);
     }
-    void Update() {
+    void FixedUpdate() {
         if (Time.time - _latestDirectionChangeTime > _directionChangeTime) {
             _latestDirectionChangeTime = Time.time;
-            CalcuateNewMovementVector(false);
+            CalcuateNewMovementVector();
         }
         transform.position = new Vector2(transform.position.x + (_movementPerSecond.x * Time.deltaTime),
             transform.position.y + (_movementPerSecond.y * Time.deltaTime));
+
+        // if (transform.localPosition.x <= 15 ) {
+        //     _latestDirectionChangeTime = Time.time;
+        //     CalcuateNewMovementVector();
+        // };
     }
 
-    private void OnTriggerStay2D(Collider2D col) {
-        if (col.name == "AiWalls") {
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.name == "AiWalls") {
             Debug.Log("reverse");
-            _movementDirection = -_movementDirection;
-            _movementPerSecond = _movementDirection * (_characterVelocity + 2);
-            _latestDirectionChangeTime = Time.time;
+      
+
+            Vector2 inDirection = _movementDirection;
+            Vector2 inNormal = collision.contacts[0].normal;
+            Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal);
+            _movementDirection = newVelocity;
+            _movementPerSecond = new Vector2(_movementDirection.x * 5, _movementDirection.y); ;
+            _latestDirectionChangeTime = 0;
         }
+        //
+        // if (collision.gameObject.tag == "Enemy") {
+        //     
+        // }
+            
+      
     }
 }
