e889d39838fe4d1435438097678b1abbedbb1e5a
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-21 23:37:58 +0100

    First version of slowing down and speeding up

--- Moon Patrol clone/Assets/Scripts/Effects/EnemyExplosionController.cs
+++ Moon Patrol clone/Assets/Scripts/Effects/EnemyExplosionController.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 
 public class EnemyExplosionController : MonoBehaviour {
+    [SerializeField] private Sprite explosionEffect;
     private void Start() {
+        Instantiate(explosionEffect);
         Destroy(gameObject, 0.5f);
     }
 }
--- Moon Patrol clone/Assets/Scripts/Vehicle/VehicleTireController.cs
+++ Moon Patrol clone/Assets/Scripts/Vehicle/VehicleTireController.cs
@@ -4,27 +4,90 @@ public class VehicleTireController : MonoBehaviour {
     [SerializeField] private Rigidbody2D tireRigidbody2D;
     [SerializeField] private PlayerParamsSO playerParams;
 
-    private float _jumpTime, _pos = 0f;
+    // TODO get keys from scriptable object keyboard control
+
+    private float
+        _jumpTime,
+        _pos,
+        _playerSpeed;
+
+    private const float 
+        MINSpeed = 4f,
+        MAXSpeed = 6f,
+        SpeedStep = 2f;
+
+
+    private bool _isJumping;
 
     private void Start() {
+        _playerSpeed = playerParams.playerSpeed;
         _pos = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down)).collider.transform
             .position.y;
     }
 
     private void Update() {
         Jump();
+        Move();
+    }
+
+    private void Move() {
+        SlowDown();
+        SpeedUp();
+        MoveForward();
     }
 
     private void Jump() {
-        if (Input.GetKeyDown(KeyCode.D) && Time.time > _jumpTime + playerParams.jumpTimeInAir) {
-            _jumpTime = Time.time + playerParams.jumpTimeInAir;
-        }
-        else if (Time.time < _jumpTime) {
-            Debug.Log($"COOLDOWN: {_jumpTime - Time.time}");
-        }
+        bool ifPressedJumpAndReadyToJump =
+            Input.GetKeyDown(KeyCode.D) && Time.time > _jumpTime + playerParams.jumpTimeInAir;
 
-        tireRigidbody2D.velocity = Time.time < _jumpTime
-            ? new Vector2(playerParams.playerSpeed, playerParams.jumpHeightAccelerate)
-            : new Vector2(playerParams.playerSpeed, _pos);
+        if (ifPressedJumpAndReadyToJump) _jumpTime = Time.time + playerParams.jumpTimeInAir;
+
+        CheckIfVehicleIsStillInAir();
+    }
+
+    private void CheckIfVehicleIsStillInAir() {
+        bool ifVehicleIsStillInAir = Time.time < _jumpTime;
+        if (ifVehicleIsStillInAir) {
+            Debug.Log($"VEHICLE IN AIR FOR: {_jumpTime - Time.time} SECONDS");
+            _isJumping = true;
+            tireRigidbody2D.velocity = new Vector2(_playerSpeed, playerParams.jumpHeightAccelerate);
+        }
+        else _isJumping = false;
+    }
+
+    private void SpeedUp() {
+        bool isSlowerThanMaxSpeed = _playerSpeed <= MAXSpeed;
+
+        if (Input.GetKey(KeyCode.R)) {
+            if (isSlowerThanMaxSpeed) {
+                Debug.Log($"min speed: {_playerSpeed}");
+                _playerSpeed += Time.deltaTime * SpeedStep;
+            }
+        }
+    }
+
+    private void SlowDown() {
+        bool ifFasterThanMinimalSpeed = _playerSpeed >= MINSpeed;
+
+        if (Input.GetKey(KeyCode.E) && ifFasterThanMinimalSpeed) {
+            Debug.Log($"min speed: {_playerSpeed}");
+            _playerSpeed -= Time.deltaTime * SpeedStep;
+            tireRigidbody2D.velocity = new Vector2(_playerSpeed, _pos);
+        }
+    }
+
+    private void MoveForward() {
+        if (_isJumping == false) tireRigidbody2D.velocity = new Vector2(_playerSpeed, _pos);
+    }
+
+    // If player not holding button anymore
+    private void BackToNormalSpeed() {
+        bool ifFasterThanNormal = _playerSpeed >= playerParams.playerSpeed;
+        bool ifSlowerThanNormal = _playerSpeed <= playerParams.playerSpeed;
+
+        if (_playerSpeed > playerParams.playerSpeed) {
+            while (_playerSpeed == playerParams.playerSpeed) _playerSpeed += Time.deltaTime * SpeedStep;
+            Debug.Log($"{_playerSpeed} speeding up");
+        }
     }
 }
