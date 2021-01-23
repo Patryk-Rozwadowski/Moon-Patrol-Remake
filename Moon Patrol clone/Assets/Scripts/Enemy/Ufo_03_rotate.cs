a3c7b672b1be9baabcbfe64e734bd05675d6e5fa
darkezowsky <darkezowsky@gmail.com>
2021-01-23 02:16:55 +0100

    FinalAssetsTestground Update + VHS Camera postprocessing

---
+++ Moon Patrol clone/Assets/Scripts/Enemy/Ufo_03_rotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0, 0, 180 * Time.deltaTime); //rotates 180 degrees per second around z axis
+    }
+}
