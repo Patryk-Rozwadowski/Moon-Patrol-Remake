5387976bd90375b64acef4b3ee8136b9f9005c8e
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-24 23:37:47 +0100

    Created wave manager with starter

---
+++ Moon Patrol clone/Assets/Scripts/Enemy/RespawnController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnController : MonoBehaviour {
+    [SerializeField] private List<GameObject> ufoWave = new List<GameObject>();
+
+    private Transform _aiWalls;
+   
+    // Update is called once per frame
+    void Update() {
+    }
+}
---
+++ Moon Patrol clone/Assets/Scripts/Enemy/WaveManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy {
+    public class WaveManager : MonoBehaviour {
+        [SerializeField] private List<GameObject> ufoWave = new List<GameObject>();
+        private Transform _aiWalls;
+        private void OnTriggerEnter2D(Collider2D other) {
+            Debug.Log(other);
+            _aiWalls = GameObject.Find("AiWalls").transform;
+            foreach (var o in ufoWave) {
+                var ufo = Instantiate(o, transform.position, Quaternion.identity);
+                ufo.transform.parent = _aiWalls;
+            }
+        }
+    }
+}
