e2d9ce041b45625674b23525c5cc0491181a0fd2
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-24 13:00:44 +0100

    Created collision controller for enemy projectiles

---
+++ Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyProjectileCollisionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using Vehicle;
+
+public class EnemyProjectileCollisionController : MonoBehaviour {
+    [SerializeField] private GameObject explosionEffect;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        var ground = other.GetComponent<SpriteShapeController>();
+        if (ground != null) {
+            var explosionEffectObject = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(explosionEffectObject, 0.2f);
+            Destroy(gameObject);
+        }
+
+        var playerVehicle = other.GetComponent<VehicleController>();
+        if (playerVehicle != null) playerVehicle.PlayerDeath();
+
+        // TODO --player life
+    }
+}
--- Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyVerticalRocketController.cs
+++ Moon Patrol clone/Assets/Scripts/Projectiles/Enemy/EnemyVerticalRocketController.cs
@@ -1,35 +1,16 @@
-﻿using Enemy;
-using UnityEngine;
-using UnityEngine.U2D;
-using Vehicle;
+﻿using UnityEngine;
 
 namespace Projectiles.Enemy {
     public class EnemyVerticalRocketController : MonoBehaviour {
-        [SerializeField] private GameObject explosionEffect;
-        
-        private Rigidbody2D rb2D;
+        private Rigidbody2D _rb2D;
         private float thrust = 2;
 
         void Start() {
-            rb2D = gameObject.GetComponent<Rigidbody2D>();
+            _rb2D = gameObject.GetComponent<Rigidbody2D>();
         }
 
         private void Update() {
-            rb2D.AddForce(transform.right * thrust);
-        }
-
-        private void OnTriggerEnter2D(Collider2D other) {
-            var ground = other.GetComponent<SpriteShapeController>();
-            if (ground != null) {
-                var explosionEffectObject = Instantiate(explosionEffect, transform.position, Quaternion.identity);
-                Destroy(explosionEffectObject, 0.2f);
-                Destroy(gameObject);
-            }
-
-            var playerVehicle = other.GetComponent<VehicleController>();
-            if(playerVehicle != null) playerVehicle.PlayerDeath();
-            
-            // TODO --player life
+            _rb2D.AddForce(transform.right * thrust);
         }
     }
 }
