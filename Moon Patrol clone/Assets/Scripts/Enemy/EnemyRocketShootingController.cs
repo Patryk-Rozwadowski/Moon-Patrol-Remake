620871ae76ede7f3100d177155b4ae086268d1b7
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-24 19:05:31 +0100

    Add logger for chances in shooting rocket, fixed prefab yellow ufo

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyRocketShootingController.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyRocketShootingController.cs
@@ -32,13 +32,16 @@ namespace Enemy {
             _randomTime = Random.Range(_minTime, _maxTime);
             _randomTime = Mathf.Round(_randomTime);
 
-            Debug.Log($"Shoot chance: {_rocketShootChance}%. Rocket will be launch in {_randomTime}");
+            Debug.Log($"Treshold for shoot chance {_rocketShootTreshold}% " +
+                      $"Shoot chance: {_rocketShootChance}%. " +
+                      $"{(_rocketShootTreshold < _rocketShootChance ? $"Rocket will be launch in {_randomTime}" : "Rocket will be not launched.")}");
         }
 
         void Update() {
             if (!(_rocketShootTreshold < _rocketShootChance) || _rocketLaunched) return;
             if (!(Time.time > _randomTime) || _rocketLaunched) return;
             _rocketLaunched = true;
+            Debug.Log($"{gameObject.name} launched rocket.");
             Instantiate(enemyBulletRocket, firepoint.transform.position, Quaternion.identity);
         }
     }
