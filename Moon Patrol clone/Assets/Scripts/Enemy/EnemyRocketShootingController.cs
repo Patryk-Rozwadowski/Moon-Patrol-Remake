d3bcace9757a39db2da94bfedb65e5aabcd03740
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-24 18:28:50 +0100

    Add basic random time and chance for ufo to shoot rocket

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyRocketShootingController.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyRocketShootingController.cs
@@ -10,14 +10,28 @@ namespace Enemy {
         private GameObject _firepointTransform;
         private ScoreManager _scoreManager;
 
+        // Chances % that ufo will shoot with rocket
+        private float _rocketShootChance;
+
+        // Random time when ufo can shoot with rocket
+        private float _randomTime;
+        
+        // Minimal and maximal time between ufo can shoot
+        private float _minTime = 2f, _maxTime = 6f;
         void Start() {
             _scoreManager = GetComponent<ScoreManager>();
             // Instantiate(enemyBulletRocket, firepoint.transform.position, Quaternion.identity);
-
+            _rocketShootChance = Random.Range(0, 1f);
+            _rocketShootChance = Mathf.Round(_rocketShootChance * 100);
+            
+            _randomTime = Random.Range(_minTime, _maxTime);
+            _randomTime = Mathf.Round(_randomTime);
+            
+            Debug.Log($"Shoot chance: {_rocketShootChance}%. Rocket will be launch in {_randomTime}");
         }
-
-        // Update is called once per frame
+        
         void Update() {
+            
         }
     }
 }
