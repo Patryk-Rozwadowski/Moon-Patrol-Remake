79e418c87bd115ce0e7dabc3b0cc552ade0b61e3
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-24 18:44:01 +0100

    Colorful ufo shooting with random chances and random time betweent 2 and 8 seconds

--- Moon Patrol clone/Assets/Scripts/Enemy/EnemyRocketShootingController.cs
+++ Moon Patrol clone/Assets/Scripts/Enemy/EnemyRocketShootingController.cs
@@ -6,32 +6,40 @@ namespace Enemy {
     public class EnemyRocketShootingController : MonoBehaviour {
         [SerializeField] private GameObject enemyBulletRocket;
         [SerializeField] private GameObject firepoint;
-        
+
         private GameObject _firepointTransform;
         private ScoreManager _scoreManager;
+
+        // Chance % treshold for ufo shooting
+        private float _rocketShootTreshold = 40;
 
         // Chances % that ufo will shoot with rocket
         private float _rocketShootChance;
 
         // Random time when ufo can shoot with rocket
         private float _randomTime;
-        
+
         // Minimal and maximal time between ufo can shoot
-        private float _minTime = 2f, _maxTime = 6f;
+        private float _minTime = 2f, _maxTime = 8f;
+
+        private bool _rocketLaunched = false;
+
         void Start() {
             _scoreManager = GetComponent<ScoreManager>();
-            // Instantiate(enemyBulletRocket, firepoint.transform.position, Quaternion.identity);
             _rocketShootChance = Random.Range(0, 1f);
             _rocketShootChance = Mathf.Round(_rocketShootChance * 100);
-            
+
             _randomTime = Random.Range(_minTime, _maxTime);
             _randomTime = Mathf.Round(_randomTime);
-            
+
             Debug.Log($"Shoot chance: {_rocketShootChance}%. Rocket will be launch in {_randomTime}");
         }
-        
+
         void Update() {
-            
+            if (!(_rocketShootTreshold < _rocketShootChance) || _rocketLaunched) return;
+            if (!(Time.time > _randomTime) || _rocketLaunched) return;
+            _rocketLaunched = true;
+            Instantiate(enemyBulletRocket, firepoint.transform.position, Quaternion.identity);
         }
     }
 }
