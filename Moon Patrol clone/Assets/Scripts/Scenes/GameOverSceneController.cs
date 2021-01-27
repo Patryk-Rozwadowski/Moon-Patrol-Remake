cb85fdd92f9ef0b7af802c92663a4cf04095169c
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-28 00:59:02 +0100

    Fixed fonts for menu start, add timer for game over and back to first menu

--- Moon Patrol clone/Assets/Scripts/Scenes/GameOverSceneController.cs
+++ Moon Patrol clone/Assets/Scripts/Scenes/GameOverSceneController.cs
@@ -1,14 +1,24 @@
-﻿using ScriptableObjects.Keyboard;
-using ScriptableObjects.Scenes;
+﻿using ScriptableObjects.Scenes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Scenes {
     public class GameOverSceneController : MonoBehaviour {
         [SerializeField] private ScenesSO scenesSO;
+        [SerializeField] private UnityEngine.UI.Text timer;
+
+        private float _timer;
         private LevelController _levelController;
+        private void Start() {
+            _timer = 10;
+        }
         
         void Update() {
+            _timer -= Time.deltaTime;
+            var seconds = Mathf.FloorToInt(_timer % 60);
+            timer.text = seconds.ToString("00");
+
+            if (_timer <= 1) SceneManager.LoadScene(0);
             if (Input.anyKeyDown) {
                 SceneManager.LoadScene($"{scenesSO.currentLevel}");
             }
