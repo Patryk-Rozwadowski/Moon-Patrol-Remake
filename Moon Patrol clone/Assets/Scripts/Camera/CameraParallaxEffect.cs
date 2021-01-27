1dd462f1a282feb7e3b01aee831f7a6a90e4ad24
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-27 18:56:36 +0100

    Refactor CameraParallaxEffect script

--- Moon Patrol clone/Assets/Scripts/Camera/CameraParallaxEffect.cs
+++ Moon Patrol clone/Assets/Scripts/Camera/CameraParallaxEffect.cs
@@ -2,18 +2,20 @@
 
 namespace Camera {
     public class CameraParallaxEffect : MonoBehaviour {
-        public GameObject cam;
-        public float parallaxEffect;
+        [SerializeField] private float parallaxEffect;
+
+        private GameObject _cam;
         private float length, startpos;
     
         private void Start() {
+            _cam = GameObject.Find("Main Camera").gameObject;
             startpos = transform.position.x;
             length = GetComponent<SpriteRenderer>().bounds.size.x;
         }
     
         private void FixedUpdate() {
-            float temp = cam.transform.position.x * (1 - parallaxEffect);
-            float dist = cam.transform.position.x * parallaxEffect;
+            var temp = _cam.transform.position.x * (1 - parallaxEffect);
+            var dist = _cam.transform.position.x * parallaxEffect;
 
             var position = transform.position;
             position = new Vector3(startpos + dist, position.y, position.z);
