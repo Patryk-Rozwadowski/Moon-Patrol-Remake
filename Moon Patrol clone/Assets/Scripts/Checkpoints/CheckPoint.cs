2adad18902d1145816e4a17b9e9a917ca4a416e0
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-01-27 21:15:11 +0100

    Fix checkpoint error

---
+++ Moon Patrol clone/Assets/Scripts/Checkpoints/CheckPoint.cs
@@ -0,0 +1,16 @@
+﻿using Scenes;
+using UnityEngine;
+using Vehicle;
+
+namespace Checkpoints {
+    public class CheckPoint : MonoBehaviour {
+        private void OnTriggerEnter2D(Collider2D other) {
+            var vehicle = other.GetComponent<VehicleController>();
+            if (vehicle != null) {
+                var sceneController = GameObject.Find("LevelController").GetComponent<LevelController>();
+                Debug.Log("next level");
+                sceneController.NextLevel();
+            }
+        }
+    }
+}
