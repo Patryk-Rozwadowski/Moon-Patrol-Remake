06e890014e9f445a258f64486113942838803cab
Antos112 <antosiewiczmi@gmail.com>
2021-01-29 16:31:52 +0100

    Added final checkpoint sound script and implemented across all scenes

---
+++ Moon Patrol clone/Assets/Scripts/Checkpoints/FinalCheckpointSound.cs
@@ -0,0 +1,20 @@
+﻿using Vehicle;
+using UnityEngine;
+using ScriptableObjects.Obstacles;
+using Score;
+
+public class FinalCheckpointSound : MonoBehaviour
+{
+    public Collider2D jumpOverCollider2D;
+    private bool isColliderIsUntouched = true;
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        VehicleController vehicle = obj.GetComponent<VehicleController>();
+        if (vehicle != null)
+        {
+            FindObjectOfType<AudioManager>().Play("LastPoint");
+            isColliderIsUntouched = false;
+        }
+    }
+}
