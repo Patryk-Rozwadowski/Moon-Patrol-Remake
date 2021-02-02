993e5a0a819d7ae0c727a24cf5d5e99d79163daf
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-02-03 00:08:39 +0100

    refactor: remove unused BaseRespawn's components and scripts

--- Moon Patrol clone/Assets/Scripts/Checkpoints/BaseRespawnSO.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace Checkpoints {
-    [CreateAssetMenu(menuName = "ScriptableObjects/BaseRespawn")]
-    public class BaseRespawnSO : ScriptableObject {
-        [SerializeField] public Sprite baseSprite;
-    }
-}
