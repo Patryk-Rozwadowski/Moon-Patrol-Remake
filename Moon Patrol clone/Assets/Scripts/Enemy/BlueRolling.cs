10d074d83463073e7e715c28b98bb7be2f73632d
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-02-08 22:51:16 +0100

    refactor: remove unused files

---
+++ Moon Patrol clone/Assets/Scripts/Enemy/BlueRolling.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class BlueRolling : MonoBehaviour {
+    void Update() {
+        transform.Rotate(0, 0, 180 * Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+}
